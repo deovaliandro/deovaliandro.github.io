4565f7edb87fef00168f9f9e4ce56a0fd32f15cb
Deo Valiandro. M <mdv16h@gmail.com>
2026-07-06 19:37:48 +0800

    test sign
    Signed-off-by: Deo Valiandro. M <mdv16h@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,17 @@
-fontFamily: {
-  sans: ['Inter', 'sans-serif'],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./layouts/**/*.html",
+    "./content/**/*.md",
+    // jika Anda menggunakan tema eksternal di folder themes:
+    "./themes/**/layouts/**/*.html",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
 }
